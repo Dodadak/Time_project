7ddbcba73529848e2fba630e2d618956bb5c9f83
Dodadak <109807724+Dodadak@users.noreply.github.com>
2024-12-11 17:35:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,4 @@ def recommend():
     return render_template('Recommend.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
